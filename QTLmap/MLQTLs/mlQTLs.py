146104e067e843b01034d97a61bb13bc6ce71e75
aariani <aariani@ucdavis.edu>
2017-11-09 19:28:33 -0800

    Implemented mlQTLs, need to optimize

--- QTLmap/MLQTLs/mlQTLs.py
+++ QTLmap/MLQTLs/mlQTLs.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.preprocessing import Imputer
 from sklearn.utils import resample
 from sklearn.model_selection import GridSearchCV
-from sklearn.linear_model import LassoCV
+from sklearn.svm import LinearSVR
 
 def impute_data(X):
     '''
@@ -19,27 +19,32 @@ def impute_data(X):
 def optimize_weights(X, y, cv):
     # for now just optimize for regression
     # for Lasso regression you need to specify the alphas
-    C_values = np.logspace(-5, 4, 10)
-    alphas =1/C_values
-    model = LassoCV(alphas=alphas, random_state=42)
+    param = {'C':np.logspace(-5, 4, 10)}
+    svr = LinearSVR(random_state=42)
+    model = GridSearchCV(svr, param_grid = param,
+                         scoring='neg_mean_squared_error', cv=10)
     model.fit(X,y)
-    return model.coef_
+    model_final = model.best_estimator_
+    model_final.fit(X,y)
+    return model_final.coef_
 
-def mlQTLs_analysis(X, y, model, n_estimators=1000,
+def mlQTLs_analysis(X, y, n_estimators=1000,
                     max_samples = 0.8, cv = 10, random_state=42):
     '''main function that will call the other functions'''
-    all_scores = []
-    x=random_state
+    all_w = []
     for i in range(n_estimators):
         # loop over the number of estimators
-        X_res, y_res = resample(X, y, n_samples=np.ceil(X.shape[0]*max_samples),
-                                replace=True, random_state=x+1)
+        X_res, y_res = resample(X, y,
+                                n_samples=int(np.ceil(X.shape[0]*max_samples)),
+                                replace=True, random_state=i*random_state)
         w = optimize_weights(X_res, y_res, cv=cv)
         all_w.append(w)
     # now you will need to:
         #1) Calculate the stability score for each feature
         #2) Sum the scores
+        #3) for optimization purpose I will return the full  coeff_array
     all_w = np.array(all_w)
     sum_w = np.sum(all_w, axis=0)
     bin_matrix = np.where(all_w>0, 1, 0)
-    s_scores = np.sum(bin_matrix, axis=1)/n_estimators
+    s_scores = np.sum(bin_matrix, axis=0)
+    return all_w, s_scores
